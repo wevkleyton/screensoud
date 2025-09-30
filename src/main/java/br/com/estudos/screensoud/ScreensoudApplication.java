52b59682bdf750314a61ec657c2f817008b601b1
wev-kleyton <kleyton.wev@gmail.com>
2025-09-30 14:08:39 -0300

    Criacao api ScreenSound

---
+++ src/main/java/br/com/estudos/screensoud/ScreensoudApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.screensoud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreensoudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreensoudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudos/screensoud/ScreensoudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.screensoud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreensoudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
